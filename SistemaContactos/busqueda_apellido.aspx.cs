0e207e6a4363e732c42d2876efdb79b0ef60dc20
rurtubiac <randy.urtubia@inacapmail.cl>
2015-09-02 22:09:08 -0300

    Added aspx files & class

---
+++ SistemaContactos/busqueda_apellido.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaContactos
+{
+    public partial class busqueda_apellido : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
